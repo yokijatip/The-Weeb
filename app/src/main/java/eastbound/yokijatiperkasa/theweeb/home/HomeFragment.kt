8639754fa9248e2eb5160e8bcba1c2f2c3da0dc7
Yoki Jati Perkasa <yokijati@gmail.com>
2024-06-04 11:52:15 +0700

    Save

--- app/src/main/java/eastbound/yokijatiperkasa/theweeb/home/HomeFragment.kt
+++ app/src/main/java/eastbound/yokijatiperkasa/theweeb/home/HomeFragment.kt
@@ -5,21 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import eastbound.yokijatiperkasa.theweeb.R
+import eastbound.yokijatiperkasa.theweeb.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding =FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvTopAnimeSeeAll.setOnClickListener {
+            Toast.makeText(requireContext(), "Waduh kamu klik aku 🫵😭", Toast.LENGTH_SHORT).show()
+        }
+
+
+
     }
 
     companion object {
